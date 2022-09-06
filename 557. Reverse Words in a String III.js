fe9a03bf587b08d9b7e8f4cfe964bcf8e305a879
muzaffer <muzafferckaplan@gmail.com>
2022-09-06 20:52:15 +0300

    leet code question 557 solved

---
+++ 557. Reverse Words in a String III.js
@@ -0,0 +1,22 @@
+/* Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD" */
+
+var reverseWords = function (s) {
+  let result = [];
+  let array = s.split(" ");
+  for (let i = 0; i < array.length; i++) {
+    let a = array[i].split("").reverse().join("");
+    result.push(a);
+  }
+  return result.join(" ");
+};
