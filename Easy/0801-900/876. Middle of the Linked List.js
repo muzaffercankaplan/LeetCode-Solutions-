8182429cd597e97efdb5b083fade39803ece80c8
muzaffer <muzafferckaplan@gmail.com>
2022-09-07 14:10:46 +0300

    leet code question 876 solved

---
+++ Easy/0801-900/876. Middle of the Linked List.js
@@ -0,0 +1,39 @@
+/* Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one. */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let a = head;
+  let b = head;
+  while (b !== null && b.next !== null) {
+    a = a.next;
+    b = b.next.next;
+  }
+  return a;
+};
