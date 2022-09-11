19ba7904c0a455170d0216e66bcee37249ba56c3
muzaffer <muzafferckaplan@gmail.com>
2022-09-11 22:37:17 +0300

    leet code question 116 solved

---
+++ Medimum/0101-200/116. Populating Next Right Pointers in Each Node.js
@@ -0,0 +1,54 @@
+/* You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+Example 2:
+
+Input: root = []
+Output: [] */
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  let res = root;
+  while (root) {
+    let head = root;
+    let next = head.left;
+    if (!next) break;
+    while (head) {
+      head.left.next = head.right;
+      head.right.next = head.next ? head.next.left : null;
+      head = head.next;
+    }
+    root = next;
+  }
+  return res;
+};
