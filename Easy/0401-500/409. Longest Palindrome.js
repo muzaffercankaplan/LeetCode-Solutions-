90e02bf53f68f9bbf85059279c943b2793fdc261
muzaffer <muzafferckaplan@gmail.com>
2022-09-12 22:09:34 +0300

    leet code question 409 solved

--- Easy/0401-500/409. Longest Palindrome.js
+++ Easy/0401-500/409. Longest Palindrome.js
@@ -14,3 +14,30 @@ Example 2:
 Input: s = "a"
 Output: 1
 Explanation: The longest palindrome that can be built is "a", whose length is 1. */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s.length;
+
+  let obj = {};
+  let oddFound = false;
+  let result = 0;
+
+  for (let letter of s) {
+    obj[letter] = obj[letter] + 1 || 1;
+  }
+
+  for (let num of Object.values(obj)) {
+    if (num % 2 === 0) result += num;
+    else {
+      oddFound = true;
+      result += num - 1;
+    }
+  }
+
+  if (oddFound) return result + 1;
+  return result;
+};
